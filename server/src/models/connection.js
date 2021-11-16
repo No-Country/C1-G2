156b5686a4404412b2867c1c085f5dc7f160e967
c0dejohn <jemanyoma07@gmail.com>
2021-11-15 23:19:35 -0500

    check health config

---
+++ server/src/models/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('../config/globals');
+const { logger } = require('../utils/logger');
+exports.getConnection = async () => {
+  // mongoose.set('useCreateIndex', true)
+  await mongoose.connect(MONGO_URI, {
+    // dbName: 'pet-adoption',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  logger.info('DB connected');
+};

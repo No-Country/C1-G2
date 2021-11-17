781b3ea583d67c6465f54f249b1bbbd6a895ab78
Jaime Ricardo Gonzalez Montenegro <jrgonzalez@sofrecom.local>
2021-11-17 18:41:48 -0300

    angular front environment

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { FullComponent } from './components/dashboard-admin/full/full.component';
+
+export const Approutes: Routes = [
+  {
+    path: 'admin',
+    component: FullComponent,
+    children: [
+      { path: '', loadChildren: () => import('./components/dashboard-admin/component.module').then(m => m.ComponentsModule) },
+      { path: '**', redirectTo: '', pathMatch: 'full' },
+    ]
+  },
+  { path: 'home', loadChildren: () => import('./components/user-landing/user-landing.module').then(m => m.UserLandingModule) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];

9300bdc32948b7e7875fe5e174a876c21c5c20df
john <jemanyoma07@gmail.com>
2021-11-23 19:24:34 -0500

    updated app & configs

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const PREFIX = '/api/pet-adoption/';
+const PREFIX = '/api/pet_adoption/';
 const app = express();
 const cors = require('cors');
 const { logger } = require('./src/utils/logger');
@@ -10,6 +10,7 @@ const corsOptions = {
 const { PORT } = require('./src/config/index');
 const petsAdoptionRouter = require('./src/routes/pets.adoption.routes');
 const usersRouter = require('./src/routes/users.routes');
+const { getConnection } = require('./src/models/connection');
 
 //middlewares
 app.use(morgan('dev'));
@@ -18,7 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
 //routes
-
 app.use(PREFIX, petsAdoptionRouter);
 app.use(PREFIX, usersRouter);
 
@@ -26,6 +26,10 @@ app.get(`${PREFIX}health`, (req, res) => {
   res.status(200).json({ status: 'OK' });
 });
 
-app.listen(PORT, async () => {
-  logger.info(`Listening on port: http://localhost:${PORT}`);
-});
+getConnection()
+  .then(() => {
+    app.listen(PORT, () => {
+      logger.info(`Listening on port: http://localhost:${PORT}`);
+    });
+  })
+  .catch(logger.error);

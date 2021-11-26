d8d393e40e6dcd5fb893f94fc182352d26f6ab86
john <jemanyoma07@gmail.com>
2021-11-26 13:21:33 -0500

    fix:conflict

--- server/app.js
+++ server/app.js
@@ -8,8 +8,10 @@ const corsOptions = {
   origin: '*',
 };
 const { PORT } = require('./src/config/index');
-const petsRouter = require('./src/routes/pets.routes');
+
 const usersRouter = require('./src/routes/users.routes');
+const petsRouter = require('./src/routes/pet.routes');
+const ongRouter = require('./src/routes/ong.router');
 const { getConnection } = require('./src/models/connection');
 
 //middlewares
@@ -19,8 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
 //routes
-app.use(PREFIX, petsRouter);
 app.use(PREFIX, usersRouter);
+app.use(PREFIX, petsRouter);
+app.use('/ong/', ongRouter);
 
 app.get(`${PREFIX}health`, (req, res) => {
   res.status(200).json({ status: 'OK' });

84fdff3ec3da93500b970259472dbe837bc47ef1
Gonzalo Moreno <morenogonza@hotmail.com>
2021-11-30 23:10:38 -0300

    corregido modulo upload-image

--- client/src/app/shared/upload-image/upload-image.module.ts
+++ client/src/app/shared/upload-image/upload-image.module.ts
@@ -3,11 +3,9 @@ import { CommonModule } from "@angular/common";
 import { UploadImageComponent } from "./upload-image.component";
 import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [{ path: "", component: UploadImageComponent }];
-
 @NgModule({
   declarations: [UploadImageComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule],
   exports: [UploadImageComponent],
 })
 export class UploadImageModule {}

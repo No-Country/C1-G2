8e1061bb632d81885f23df0ee4e99058eb58f331
Alacran86 <alacran.mlr@gmail.com>
2021-12-06 16:12:02 -0300

    contact

--- client/src/app/shared/contact-form/contact-form.component.ts
+++ client/src/app/shared/contact-form/contact-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import Swal from "sweetalert2";
+import { Router } from '@angular/router';
+
+// import { RegistroService } from './registro.service';
 
 @Component({
   selector: "app-contact-form",
@@ -6,7 +11,44 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./contact-form.component.css"],
 })
 export class ContactFormComponent implements OnInit {
-  constructor() {}
+  public form: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      name: new FormControl(null, [
+        Validators.required
+      ]),
+      email: new FormControl(null, [
+        Validators.required
+      ]),
+      asunto: new FormControl(null, [
+        Validators.required
+      ]),
+      mensaje: new FormControl(null, [
+        Validators.required
+      ])
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  public saveForm(): void {
+    console.log(this.form);
+  }
+
+  send(): void {
+    const { name, email, asunto, mensaje } = this.form.value;
+
+    // // this.authService.mail(username, password).then( (resp) => {
+    // //   if(resp){
+    // //     this.router.navigateByUrl('home');
+    // //   }
+    // // }).catch( ({error}) => {
+    // //   Swal.fire("Error", error.msg, "error");
+    // })
+  }
 }
--- client/src/app/shared/contact-form/contact-form.module.ts
+++ client/src/app/shared/contact-form/contact-form.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContactFormComponent } from './contact-form.component';
+
 
 
 @NgModule({
   declarations: [ContactFormComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    FormsModule
 ],
 exports: [ContactFormComponent]
 

2d2a554f1a1933eea58e648f4ac01e9619b50f3b
Alacran86 <alacran.mlr@gmail.com>
2021-12-07 18:35:20 -0300

    reactive

--- client/src/app/shared/contact-form/contact-form.component.ts
+++ client/src/app/shared/contact-form/contact-form.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import Swal from "sweetalert2";
 import { Router } from '@angular/router';
 
-// import { RegistroService } from './registro.service';
+// import { ContactoService } from './contacto.service';
 
 @Component({
   selector: "app-contact-form",
@@ -17,11 +17,11 @@ export class ContactFormComponent implements OnInit {
     private fb: FormBuilder,
     private router: Router
     ) {
-      this.form = new FormGroup({
-        name: new FormControl('', [Validators.required]),
-        email: new FormControl('', [Validators.email]),
-        subject: new FormControl('', [Validators.maxLength(200)]),
-        message: new FormControl('', [Validators.required]),
+      this.form = this.fb.group({
+        name: new FormControl(null, [Validators.required]),
+        email: new FormControl(null, [Validators.email]),
+        subject: new FormControl(null, [Validators.maxLength(200)]),
+        message: new FormControl(null, [Validators.required]),
       });
     }
 
@@ -36,7 +36,7 @@ export class ContactFormComponent implements OnInit {
   saveContact(): void {
     const { name, email, subject, message} = this.form.value;
 
-    // this.register( name, email, subject, message).then( (resp) => {
+    // this.contactoService.saveContact( name, email, subject, message).then( (resp) => {
     //   if(resp){
     //     this.router.navigateByUrl('admin');
     //   }

6ddc219faf2dc26b5bc363a6dafe83d3786fcaf0
Alacran86 <alacran.mlr@gmail.com>
2021-12-09 20:14:04 -0300

    routes

--- server/src/routes/form.routes.js
+++ server/src/routes/form.routes.js
@@ -2,14 +2,19 @@ const express = require('express');
 const Router = express.Router();
 const mail = require('../services/mail.service');
 
-Router.post('/', async function (req, res)  {
+Router.post('/', async function (req, res) {
+    try {
 
     const texto = `Nombre: ${req.body.name} \nMotivo: ${req.body.subject} \nEmail: ${req.body.email} \nMensaje: ${req.body.message}`;
 
     const info =  await mail.main(texto);
 
-    res.send('Mail enviado correctamente');
-  
+    res.send(info);
+        
+    } catch (error) {
+        console.error(error);
+    }
+
 });    
     
 

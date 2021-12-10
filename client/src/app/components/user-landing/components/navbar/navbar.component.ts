2be67a358ded92383c3abc217c7c8f28c96f4273
Jaime Ricardo Gonzalez Montenegro <jrgonzalez@sofrecom.local>
2021-12-09 23:17:12 -0300

    some changes in a lot of components

--- client/src/app/components/user-landing/components/navbar/navbar.component.ts
+++ client/src/app/components/user-landing/components/navbar/navbar.component.ts
@@ -11,7 +11,6 @@ import { AuthService } from '../../../../services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  public auth: boolean = false;
   loggedIn: boolean = false;
 
   constructor(
